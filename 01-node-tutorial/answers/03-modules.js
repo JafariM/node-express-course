0db46e842bcd659d699a7b2df2405c63e7dec11f
JafariM <masuma.jafari@gmail.com>
2024-11-05 20:59:09 -0800

    answers for lesson 1

---
+++ 01-node-tutorial/answers/03-modules.js
@@ -0,0 +1,8 @@
+const names = require("./04-names");
+const sayHi = require("./05-utils");
+const data = require("./06-alternative-flavor");
+const myName = require("./07-mind-grenade");
+sayHi(names.Shayan);
+sayHi(names.Omid);
+console.log("Pet type is: ", data.petType);
+console.log("books are :", data.books);
---
+++ 01-node-tutorial/answers/06-alternative-flavor.js
@@ -0,0 +1,7 @@
+module.exports.books = ["book1", "book2"];
+
+const pet = {
+  type: "Dog",
+};
+
+module.exports.petType = pet;
